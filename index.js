c257c411eaa221d0473e9c8f87d1deea1bb4713e
Gilberto Medero <gjmedero@gmail.com>
2023-03-29 13:06:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (driver) {
+        if(string != driver.slice(0,string.length))
+            return false;
+            
+        return driver.includes(string);
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function (driver) {
+        return driver.name == string;
+    });
+}
